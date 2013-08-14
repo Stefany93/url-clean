96b48f1a2055729885ab7f1f8d7c6e92eac8b46b
Stefany Dyulgerova <stefany.dyulgerova@gmail.com>
2013-08-15 00:33:18 +0300

    URL cleaner
    Remove/replaces invalid characters from URLs

---
+++ url_cleaner.php
@@ -0,0 +1,10 @@
+<?php 
+// Remove/replace invalid characters in the custom URLs
+// generated from the title of the post.
+// 'Cause otherwise the URLs will look like scrambled eggs.
+	function clean_url($url){
+		$search = array('-',' ','$','%','.');
+		$replace = array('','-','','','');
+		$cleaned_url = strtolower(str_replace($search, $replace, $url));
+		return $cleaned_url;
+	}
